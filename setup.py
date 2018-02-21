c9fa9f72f5ad2450f8917031f1aadf85bee94abb
Ben Farr <bfarr@uoregon.edu>
2018-02-20 17:35:51 -0800

    Add automatic versioning to setup.py

--- setup.py
+++ setup.py
@@ -7,9 +7,14 @@ try:
 except ImportError:
     from distutils.core import setup
 
+version_re = re.compile("__version__ = \"(.*?)\"")
+with open(path.join(path.dirname(path.abspath(__file__)), "exocartographer", "__init__.py")) as inp:
+    r = inp.read()
+version = version_re.findall(r)[0]
+
 setup(
     name='exocartographer',
-    version='1.0',
+    version=version,
     description='Map some exoplanets',
     author='Ben Farr, Will Farr, Nick Cowan',
     author_email='bfarr@uoregon.edu, w.farr@bham.ac.uk, nicolas.cowan@mcgill.ca',
